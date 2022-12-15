fb186e848d63cf2eb70fd5b03dc68fed4ec0b1e2
sohail70 <sohail.e.nia@gmail.com>
2022-12-15 19:44:20 +0330

    Add dasy drop down and using state for the whole App.tsx and useEffect for the API call

--- src/App.tsx
+++ src/App.tsx
@@ -26,12 +26,16 @@ function App() {
     //Tell the useState what type to expect - deghat kun useState default esh be undefined hast pas benvis null to parantez onvaght age ternary paeen dar cryptos? ro nazari typescript mige object is possibly null ama age null ro nazari typescript mige possibly null or undefined
     const [cryptos, setCryptos] = useState<Crypto[] | null>(null); // to say that Crypto is the type and [] to say that its an array. null ham gozashtim ta begim agar chzi return nashod null hast-->in va export type Crypto bala ro nazari nemitoni .map bezani ro crypto
     const [selected, setSelected] = useState<Crypto | null>(); //null bezari to deafult value khate setSelecte(c) eror mide
+
+    const [range , setRange] = useState<number>(30); //30 days as the default
+
     const [data, setData] = useState<ChartData<'line'>>(); //jaye line mitoni har chi dost dari bezari
     const [options, setOptions] = useState<ChartOptions<'line'>>({
         responsive: true,
         plugins: {
             legend: {
-                position: 'top' as const
+                // position: 'top' as const ,
+                display: false // age mikhay legedn dashte bashi balae ro uncomment kun va in khat ro hazf kun
             },
             title: {
                 display: true,
@@ -47,24 +51,19 @@ function App() {
             setCryptos(response.data);
         });
     }, []); // Execute once on startup
-    return (
-        <>
-            <div className="App">
-                <select
-                    onChange={(e) => {
-                        // console.log(e.target.value); //value hamooni hast ke dar tag e option gozashti --> alabte age value ro nazari chi? tebghe site https://developer.mozilla.org/en-US/docs/Web/HTML/Element/select dar paragraph ghabl az section attributes gofte ke ke age value nazari default text e ke toye tag hast mire dakhele value dar e.target.value yani crypto.name e ke content tag option hast mire toye e.target.value
-                        const c = cryptos?.find((x) => x.id === e.target.value); // To get the object of the thing that you selected in the dropp down --> from id to the object
-                        // console.log(c);
-                        setSelected(c);
+
+
+    useEffect(()=>{
+            if (!selected) return; // chera? chun in useEffect dar load avalie page ke drop down roye choose an optino hast load mishe va console error mide ke chizi nabood get kune pas vase hamin in if ro gozashtim --> zeman in khat jelooye in error ro ham migire ke ghabl inke choose bokoni 7 days ya 1 day ro select kuni ke agar in khat nabashe ba ham netwrok error mide
                         axios
                             .get(
-                                `https://api.coingecko.com/api/v3/coins/${c?.id}/market_chart?vs_currency=usd&days=30&interval=daily`
+                                `https://api.coingecko.com/api/v3/coins/${selected?.id}/market_chart?vs_currency=usd&days=${range}&${range===1 ? 'interval=hourly' : 'interval=daily'}`
                             )
                             .then((response) => {
                                 console.log(response.data); //agar dar conslode check kuni mibin ye object mide ke tosh ye prices array hast ke har elementesh do ta value timestamp a price ro dare
                                 setData({ // content in json ro az https://react-chartjs-2.js.org/examples/line-chart gereftim 
                                     labels: response.data.prices.map((price: number[])=>{
-                                        return moment.unix(price[0]/1000).format("MM-DD-YYYY"); // two digit month like 06 and two digit days like 15
+                                        return moment.unix(price[0]/1000).format(range===1 ? "HH:MM" : "MM-DD-YYYY"); // two digit month like 06 and two digit days like 15
                                     }), //map faghat roye array kar mikune va data ham object hast ke tosh array e price darim
                                     datasets: [
                                         {
@@ -77,7 +76,33 @@ function App() {
                                         }
                                     ]
                                 });
+                        
+
+                        setOptions({
+                                    responsive: true,
+                                    plugins: {
+                                        legend: {
+                                            // position: 'top' as const,
+                                            display: false // age mikhay legend dashte bashi display false o hazf kun va balae ro uncomment kun
+                                        },
+                                        title: {
+                                            display: true,
+                                            text: `${selected?.name} Price Over Last ` + range + (range===1 ? ' Day.': ' Days.')
+                                        }
+                                    }
+                                })
                             });
+    },[selected , range]); // har vaght state e selected or range taghir kard in useEffect run mishe
+
+    return (
+        <>
+            <div className="App">
+                <select
+                    onChange={(e) => {
+                        // console.log(e.target.value); //value hamooni hast ke dar tag e option gozashti --> alabte age value ro nazari chi? tebghe site https://developer.mozilla.org/en-US/docs/Web/HTML/Element/select dar paragraph ghabl az section attributes gofte ke ke age value nazari default text e ke toye tag hast mire dakhele value dar e.target.value yani crypto.name e ke content tag option hast mire toye e.target.value
+                        const c = cryptos?.find((x) => x.id === e.target.value); // To get the object of the thing that you selected in the dropp down --> from id to the object
+                        // console.log(c);
+                        setSelected(c);
                     }}
                     defaultValue="default"
                 >
@@ -91,6 +116,13 @@ function App() {
                               );
                           })
                         : null}
+                </select>
+                <select onChange={(e)=>{
+                    setRange(parseInt(e.target.value)); // agar parseInt ro nazari error mide chun e.target.value string mide biroon age mikhay string e bashe value haye paeen ro string bede va useState vase range ro ham string bede  na number
+                }}>
+                          <option value={30}>30 Days</option>
+                          <option value={7}>7 Days</option> 
+                          <option value={1}>1 Day</option>
                 </select>
             </div>
             {selected ? <CryptoSummary crypto={selected} /> : null}
